72fddaf8ed87121844173917d82080bd6a6e3143
kitan <alabiridolakitan487@gmail.com>
2024-12-10 12:29:38 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//Adding three numbers
+var num1=17
+var num2=25
+var num3=12
+var sum= num1+ num2+ num3
+console.log(sum)
+
+
+//Find percentage Profit
+var cp= 12500
+var sp= 72750
+
+//Selection Processing
+var age= 17
+if(age>17){
+    console.log("You can vote")
+} else{
+    console.log("You can’t vote")
+} 
+
